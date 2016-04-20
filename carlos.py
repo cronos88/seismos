2164ceb6a98702e56c124b31caf4f79c24dc0fb4
Carlos Millan <atlantis9@hotmail.com>
2016-04-19 21:11:18 -0500

    Update carlos.py

--- carlos.py
+++ carlos.py
@@ -14,10 +14,9 @@ os.system('cls')
 response = requests.get("http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_day.geojson")
 data = response.json()
 
-"""
-En http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php está la estructura
-de los datos del GeoJSON
-"""
+#En http://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php está la estructura
+#de los datos del GeoJSON
+
 for sismo in data['features']:
 	lugar = sismo['properties']['place']
 	mag = sismo['properties']['mag']
@@ -27,10 +26,7 @@ for sismo in data['features']:
 	dif = tiempo_ahora - tiempo_sismo
 	prof = sismo['geometry']['coordinates']
 	
-        """
-	Utilizo la librería llamada Colorama para mostrar con colores los
-        sismos cuya intensidad esté en un rango cualquiera.
-	"""
+    #Utilizo la librería llamada Colorama para mostrar con colores los sismos cuya intensidad esté en un rango cualquiera."""
 	if mag<4.0:
 	    print(Fore.WHITE+"Hace:{0}h:{1}m".format(dif.seconds//3600, (dif.seconds//60)%60)+" 	Mag:"+str(mag)+ "  "+"Lugar: "+str(lugar)+Back.RESET)
 	    
@@ -38,8 +34,7 @@ for sismo in data['features']:
 	    print(Fore.YELLOW+"Hace:{0}h:{1}m".format(dif.seconds//3600, (dif.seconds//60)%60)+" 	Mag:"+str(mag)+ "  "+"Lugar: "+str(lugar)+Back.RESET)
 	    
 	elif mag>6:
-	    print(Fore.RED+"Hace:{0}h:{1}m".format(dif.seconds//3600, (dif.seconds//60)%60)+" 	Mag:"+str(mag)+ "  "+"Lugar: "+str(lugar)+Back.RESET)
-	    
-        elif mag>6 and prof[2] < 30.0:
-            print(Fore.WHITE+Back.RED+"Hace:{0}h:{1}m".format(dif.seconds//3600, (dif.seconds//60)%60)+" 	Mag:"+str(mag)+ "  "+"Lugar: "+str(lugar)+Back.RESET)
-            
+		print(Fore.RED+"Hace:{0}h:{1}m".format(dif.seconds//3600, (dif.seconds//60)%60)+" 	Mag:"+str(mag)+ "  "+"Lugar: "+str(lugar)+Back.RESET)
+
+	elif mag>6 and prof[2] < 30.0:
+		print(Fore.WHITE+Back.RED+"Hace:{0}h:{1}m".format(dif.seconds//3600, (dif.seconds//60)%60)+"    Mag:"+str(mag)+ "  "+"Lugar: "+str(lugar)+Back.RESET)
